f59bddf9a5f585f3b961a4b42a3ca16fc63c88d7
Viswa <viswankl40@gmail.com>
2025-06-28 16:34:25 +0530

    just small changes 2

--- musicapp/views.py
+++ musicapp/views.py
@@ -48,7 +48,7 @@ def authenticate_credentails(email,password):
     except User.DoesNotExist:
         return False
 
-def dashboard(request): 
+def dashboard(request): hghfgdfgdfdfgdgfghf
     token = request.session['jwt']
     if not token:
         return redirect('login')
