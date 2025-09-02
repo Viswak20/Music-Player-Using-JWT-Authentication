7a4b923e3deb4042d1632b5901fd981b051dd4a6
Viswa_._22 <viswankl40@gmail.com>
2025-09-02 10:41:33 +0530

    changes

---
+++ musicplayer/middleware.py
@@ -0,0 +1,27 @@
+import jwt
+from django.conf import settings
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+from django.shortcuts import redirect
+
+class JWTAuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        public_paths = ['/login/', '/signup/', '/static/','/admin/','/api/']
+        if any(request.path.startswith(path) for path in public_paths):
+            return self.get_response(request)
+
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            return redirect('login')
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            request.user_id = payload.get('user_id')
+            request.user_email = payload.get('email')
+        except (ExpiredSignatureError, InvalidTokenError):
+            return redirect('login')
+
+        return self.get_response(request)
