d2ef895c62c22d3466e4d8994efb92266d11a62a
Viswa <viswa.kalaiyarasan@indiafilings.com>
2026-04-24 17:14:21 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 cd /home/ubuntu/Music-Player-Using-JWT-Authentication
 
-# create venv if not exists
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
@@ -11,8 +10,6 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-# stop old process
 pkill -f gunicorn || true
 
-# start new app
 nohup gunicorn musicplayer.wsgi:application --bind 0.0.0.0:8000 > output.log 2>&1 &
